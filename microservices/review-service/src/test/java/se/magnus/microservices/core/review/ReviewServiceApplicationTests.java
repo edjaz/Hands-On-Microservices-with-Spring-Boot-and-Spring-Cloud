9c5c291acb3b91e72f555325f5a6e751a1c384cc
Dimitri KAHN <dimitri.kahn@edjaz.fr>
2019-11-12 23:06:36 +0100

    server-config

--- microservices/review-service/src/test/java/se/magnus/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/se/magnus/microservices/core/review/ReviewServiceApplicationTests.java
@@ -29,6 +29,7 @@ import static se.magnus.api.event.Event.Type.DELETE;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=RANDOM_PORT, properties = {
 	"eureka.client.enabled=false",
+	"spring.cloud.config.enabled=false",
     "spring.datasource.url=jdbc:h2:mem:review-db"})
 public class ReviewServiceApplicationTests {
 
